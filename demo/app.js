abb33b029922b7d9f525fbda5eab8ff2fc7e27b7
Jacek Pietal <jacek@gamecodehq.com>
2018-10-24 23:21:48 +0200

    updated nicely (loop / resize)

--- demo/app.js
+++ demo/app.js
@@ -1,43 +1,45 @@
 'use strict'
 
-import { Application, Mesh } from '../src'
-import StatsScene from './statsScene'
+import { PerspectiveCamera } from 'three'
+import { Application, Mesh, Scene } from '../src'
+import Stats from 'stats-js'
+
+const stats = new Stats()
+
+document.body.appendChild(stats.domElement)
 
 export default class App extends Application {
   constructor() {
     super()
     this.camera.position.z = 10
-    this.benchmark()
-  }
-  addEmptyScene() {
-    this.currentScene = new StatsScene()
-  }
-  createCursor() {
-    const $mouse = this.state.mouse
-    this.cursor = this.createCube(function () {
-      this.transform.position.x = $mouse.x
-      this.transform.position.y = $mouse.y
-    })
-    return this.cursor
   }
   createCube(onUpdate) {
     return new Mesh({ onUpdate })
   }
-  benchmark() {
-    setTimeout(() => {
-      const speedX = Math.random() * 0.1
-      const speedY = Math.random() * 0.2
-      const speedZ = Math.random() * 0.3
-      const cube = this.createCube(function () {
-        this.transform.rotation.x += speedX
-        this.transform.rotation.y += speedY
-        this.transform.rotation.z += speedZ
-      })
-      cube.transform.position.x = (Math.random() - 0.5) * 10
-      cube.transform.position.y = (Math.random() - 0.5) * 10
-      cube.transform.position.z = (Math.random() - 0.5) * 10
-      this.addToScene(cube)
-      this.benchmark()
-    }, 1000 / 60)
+  addCube() {
+    const speedX = Math.random() * 0.1
+    const speedY = Math.random() * 0.2
+    const speedZ = Math.random() * 0.3
+    const cube = this.createCube(function () {
+      this.transform.rotation.x += speedX
+      this.transform.rotation.y += speedY
+      this.transform.rotation.z += speedZ
+    })
+    cube.transform.position.x = (Math.random() - 0.5) * 15
+    cube.transform.position.y = (Math.random() - 0.5) * 10
+    cube.transform.position.z = 0
+    this.addToScene(cube)
+  }
+  beforeUpdate() {
+    stats.begin()
+    super.beforeUpdate()
+  }
+  loop() {
+    this.addCube()
+    super.loop()
+  }
+  afterUpdate() {
+    super.afterUpdate()
+    stats.end()
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './demo/index.js',
@@ -14,10 +13,7 @@ module.exports = {
       hash: false,
       filename: './index.html',
       template: 'demo/index.html'
-    }),
-    new CopyWebpackPlugin([
-      { from: 'demo/static', to: 'static' },
-    ], {})
+    })
   ],
   module: {
     rules: [
